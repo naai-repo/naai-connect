e3669510dd463335bd8ee6cc80d727763376abee
shubh <ssagar8186@gmail.com>
2024-11-04 21:52:56 +0530

    added code

---
+++ src/app/temp1/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+
+const page = () => {
+  return (
+    <>
+  <h1>hello this is your page</h1>
+   <div className="flex justify-center">
+   <Button>Click me</Button>
+   <Button>Click me 2</Button>
+   </div>
+    </>
+  )
+}
+
+export default page;
+
+
+
+
+    
