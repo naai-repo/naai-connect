5e5fa1db4dc4f7702a28752fe5fc8224b4c42f00
shubham0419 <shubham.e21@nsut.ac.in>
2024-11-05 22:12:26 +0530

    not fixed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  env : {
+    API_BASE_URL : process.env.API_BASE_URL ?? "https://dev.naai.in",
+  },
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  },
 };
 
 export default nextConfig;
--- src/app/salon/[id]/page.tsx
+++ src/app/salon/[id]/page.tsx
@@ -1,31 +1,47 @@
-"use client";
-
-import * as React from "react";
-import { Calendar } from "@/components/ui/calendar";
+"use client"
+import React, { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
+import { useSalonService } from "@/hooks/salonHooks";
+import { useRecoilState } from "recoil";
+import { salonLoading, singleSalonDataSelector } from "@/recoil/salon/atom";
 
 const DynamicPage = () => {
-  
   const params = useParams();
   const id = params?.id as string | null;
+  const salonService = useSalonService();
+  const [salonData,setSalonData] = useRecoilState(singleSalonDataSelector);
+  const [loading,setLoading] = useRecoilState(salonLoading);
+  const [date, setDate] = useState<Date | undefined>(new Date());
 
-  const [date, setDate] = React.useState<Date | undefined>(new Date());
+  if (!id) {
+    return <div>Loading...</div>;
+  }
+
+  useEffect(()=>{
+    setLoading(true);
+    const load = async ()=>{
+      try {
+        const res = await salonService.getSalonDataById(id);
+        setSalonData(res?.data?.data);
+      } catch (error) {
+        // naaitoast
+      } finally{
+        setLoading(false);
+      }
+    }
+    load();
+  },[id])
 
   return (
     <div className="flex flex-col items-center">
       <p>User ID: {id}</p>
       <br />
       <div className="w-full max-w-md">
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={setDate}
-          className="rounded-md border shadow w-full"
-        />
+        
       </div>
     </div>
   );
 };
 
-
 export default DynamicPage;
+
