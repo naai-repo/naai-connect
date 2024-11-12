0716f491e5d50fbea84ad4e13e41b7a4451676a8
shubham0419 <shubham.e21@nsut.ac.in>
2024-11-12 18:36:54 +0530

    booking on the way

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,11 @@ import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 
 export default function Home() {
-  const [user, setUser] = useRecoilState(userAtom);
-
-  console.log(user);
-  useEffect(() => {
-    setUser({
-      id: "123",
-      email: "test@test.com",
-    });
-  }, [setUser]);
+  
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
+      hiiii
     </div>
   );
 }
