1045f5a47a5eaa53b8f2fb104fb8a4d3b9f16a72
shubham0419 <shubham.e21@nsut.ac.in>
2024-12-10 21:00:29 +0530

    done

--- src/constants/apiConstant.ts
+++ src/constants/apiConstant.ts
@@ -1,6 +1,6 @@
 
 export default class API_CONSTANTS {
-  static BASE_URL = process.env.API_BASE_URL || "http://localhost:8800";
+  static BASE_URL = process.env.API_BASE_URL || "https://dev.naai.in";
 
   static getSalonDataById = this.BASE_URL+"/partner/salon/single/<SALON_ID>";
   static getCategories = this.BASE_URL+'/partner/service/category/all';
